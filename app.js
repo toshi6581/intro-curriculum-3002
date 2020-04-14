113fd3e588c97b0f1c7ca26fc3818315add54a8f
Toshi <toshi3arai3@gmail.com>
2020-04-14 14:04:40 +0900

    人が減った割合のランキング

--- app.js
+++ app.js
@@ -1,41 +1,57 @@
 'use strict';
 const fs = require('fs');
 const readline = require('readline');
+//モジュール読み込み
+
 const rs = fs.createReadStream('./popu-pref.csv');
-const rl = readline.createInterface({ 'input': rs, 'output': {} });
-const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
-rl.on('line', (lineString) => {
-  const columns = lineString.split(',');
-  const year = parseInt(columns[0]);
-  const prefecture = columns[1];
-  const popu = parseInt(columns[3]);
-  if (year === 2010 || year === 2015) {
-    let value = prefectureDataMap.get(prefecture);
-    if (!value) {
-      value = {
-        popu10: 0,
-        popu15: 0,
-        change: null
-      };
-    }
-    if (year === 2010) {
-      value.popu10 = popu;
-    }
-    if (year === 2015) {
-      value.popu15 = popu;
-    }
-    prefectureDataMap.set(prefecture, value);
-  }
+//popu-pref.csvファイルから、ファイルを読み込みを行う Stream（ストリーム）を生成
+
+const rl = readline.createInterface({
+    'input': rs,
+    'output': {}
+    //outputは多分{}としないといけないぽい
 });
+//さらにそれをreadline オブジェクトの input として設定し、 rl オブジェクトを作成しています。
+
+const prefectureDataMap = new Map();
+// key: 都道府県 value: 集計データのオブジェクト
+
+rl.on('line', (lineString) => {
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const prefecture = columns[1];
+    const popu = parseInt(columns[3]);
+    if (year === 2010 || year === 2015) {
+        let value = prefectureDataMap.get(prefecture);
+        if (!value) {
+            value = {
+                popu10: 0,
+                popu15: 0,
+                change: null
+            };
+        }
+        if (year === 2010) {
+            value.popu10 = popu;
+        }
+        if (year === 2015) {
+            value.popu15 = popu;
+        }
+        prefectureDataMap.set(prefecture, value);
+    }
+});
+
 rl.on('close', () => {
-  for (let [key, value] of prefectureDataMap) { 
-    value.change = value.popu15 / value.popu10;
-  }
-  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
-  });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
-  });
-  console.log(rankingStrings);
+    for (let [key, value] of prefectureDataMap) {
+        value.change = value.popu15 / value.popu10;
+    }
+    const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+        return pair1[1].change - pair2[1].change;
+    });
+    const rankingStrings = rankingArray.map(([key, value], i) => {
+        return `${key}: ${value.popu10} -> ${value.popu15} 変化率: ${value.change} は${i + 1}番目`;
+    });
+    // const rankingStrings = rankingArray.map(([key, value]) => {
+    //     return `${key}: ${value.popu10} -> ${value.popu15} 変化率: ${value.change}`;
+    // });
+    console.log(rankingStrings);
 });
